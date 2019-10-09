82734bcbae0630c6940c11080eb8c22ffc816d96
Abderrahmane Mouzoune <abderrahmane_mouzoune@carrefour.com>
2019-10-09 14:02:51 +0200

    ajout de quelque truc

---
+++ add-article.php
@@ -0,0 +1,71 @@
+<?php
+    session_start();
+    require 'include/function.php';
+
+    echo $_SESSION['myconnect'];
+    
+    // As-t'on envoyé des infos ?
+    if (isset($_POST) and !empty($_POST)) {
+        // Initialisation des variables
+        $article_titre = $_POST["article_titre"];
+        $article_photo = $_POST["article_photo"];
+        $article_contenu = $_POST["article_contenu"];
+        $id_article_categorie = $_POST["id_article_categorie"];
+        $article_createur = $_SESSION['id_user'];
+        $article_date = date("YmdHis");
+        $article_visible = $_POST["article_visible"];
+
+        $sql = "INSERT INTO article(article_titre,article_photo,article_contenu,id_article_categorie,article_createur,article_date,article_visible)
+        VAlUES ('.$article_titre.','.$article_photo.','.$article_contenu.','.$id_article_categorie.','.$article_createur.','.$article_date.','.$article_visible.')";
+
+
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Up to us</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <form method="post" action="./add-article.php" class="container my-5">
+        <div class="form-group">
+            <label for="titre">Titre</label>
+            <input type="text" class="form-control" id="titre" placeholder="Titre de l'article" name="article_titre">
+        </div>
+        <div class="form-group">
+            <label for="id_article_categorie">Catégorie</label>
+            <select class="form-control" id="id_article_categorie" name="id_article_categorie">
+                <option value="">1</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="article_photo">Example file input</label>
+            <input type="file" class="form-control-file" id="article_photo" name="article_photo">
+        </div>
+        <div class="form-group">
+            <label for="article_contenu">Contenu article</label>
+            <textarea class="form-control" id="article_contenu" rows="3" name="article_contenu"></textarea>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="article_visible" name="article_visible" value="1">
+            <label class="form-check-label" for="article_visible">On montre l'article ?</label>
+        </div>
+        <button type="submit" class="btn btn-primary col-md-10">Publier l'article</button>
+    </form>
+</body>
+
+</html>
--- include/controle-connexion.php
+++ include/controle-connexion.php
@@ -4,19 +4,19 @@ require '../includes-front/entete.php';
 
 if (!empty($_POST)) {
     # L'identifiant que le visiteur met            
-    $post_username = htmlentities($_POST['user_username'], ENT_QUOTES, "ISO-8859-1");           # On vérifie qu'il n'y a pas d'injection SQL
-    $post_username = strtolower($post_username);
+    $post_mail = htmlentities($_POST['user_mail'], ENT_QUOTES, "ISO-8859-1");           # On vérifie qu'il n'y a pas d'injection SQL
+    $post_mail = strtolower($post_mail);
     # Le mot de passe que le visiteur met
     $post_password =  htmlentities($_POST['user_password'], ENT_QUOTES, "ISO-8859-1");
     # On vérifie si le nom d'utilisateur est dans la base de données
     $mysqli = db_connect(); # On se connecte à la base de données
-    $sqlUSER = execute("SELECT COUNT(user_username) FROM user WHERE user_username='.$post_username.'");
-    echo $sqlUSER['user_username'];
+    $sqlUSER = execute("SELECT COUNT(user_mail) FROM user WHERE user_mail='.$post_mail.'");
+    echo $sqlUSER['user_mail'];
     exit;
     # S'il y a un résultat correspondant à la requête (Le nom d'utilisateur existe)  alors on récupère le mot de passe associé
     if ($sqlUSER == 1) {
         # On fait une requête pour récuperer le mot de passe en base de données
-        $sqlPASSWORD = execute("SELECT * FROM user WHERE user_username='.$post_username.'");
+        $sqlPASSWORD = execute("SELECT * FROM user WHERE user_mail='.$post_mail.'");
         # On associe la valeur du mot de passe hasher à une variable
         $user_password = $sqlPASSWORD["user_password"];
         $user_id = $sqlPASSWORD["id_user"];
