25db619db162b36742486ed37c87dedcdff32809
Abderrahmane Mouzoune <abderrahmane_mouzoune@carrefour.com>
2019-10-16 15:43:38 +0200

    add new control admin

--- include/acces_admin.php
+++ include/acces_admin.php
@@ -1,6 +1,16 @@
 <?php
+
+    $sql = "SELECT id_user_type,user_valid from user where id_user_type = '1' and user_valid='1' and id_user='$_SESSION["id_user"]'";
+    $is_admin = nbrSQL($sql);
+
+    if ($is_admin == 1) {       // il est admin
+        $is_admin = true;
+    } else {                    // il n'est admin
+        $is_admin = false;
+    }
+
     // S'il n'est pas admin
-    if (is_admin()) {
+    if (is_admin() and !$is_admin) {
         die("Vous n'êtes pas admin !");
     }
 ?>
